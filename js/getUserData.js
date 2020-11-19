ec91dc2c48acbdf12ddba8c40c38a75619d2269c
jundogown <ns8708ns@naver.com>
2020-11-19 23:38:39 +0900

    Update getUserData.js

--- js/getUserData.js
+++ js/getUserData.js
@@ -5,7 +5,8 @@ const getUserData = () => {
     let ajaxLastNum = ajaxCountNum;
 
     $.ajax({
-        url : "http://10.80.161.202:8080/user",
+        // url : "http://10.80.161.202:8080/user",
+        url : "https://coatcode.herokuapp.com/user",
         //url : 
         type : "GET",
         beforeSend : function(xhr){
@@ -16,7 +17,7 @@ const getUserData = () => {
         success : function(res){
             console.log(res);
             saveUserData(res);
-            console.log(res.image);
+            console.log(res.profile);
             // res.map((data) => { 배열형태가 아니라 object 형태임 !
                 $('.headContainer').append(`
                     <div class="head">
@@ -27,7 +28,9 @@ const getUserData = () => {
                             <input type="text" placeholder="  search">
                         </div>
                         <div class="profile">
-                            <img src="${res.image}" alt="">
+                            <a href="profileDetail.html?userId=${res.id}">
+                                <img src="${res.profile}" alt="">
+                            </a>
                         </div>
                     </div>
                     <div class="line">
@@ -55,6 +58,7 @@ const getUserData = () => {
 }
 
 const saveUserData = (res) => {
+    sessionStorage.setItem('description', res.description);
     sessionStorage.setItem('id', res.id);
     sessionStorage.setItem('email', res.email);
     sessionStorage.setItem('username', res.username);
