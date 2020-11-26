4c09705f3e87c05924898e349831be45c3ee741c
jundogown <ns8708ns@naver.com>
2020-11-26 11:16:48 +0900

    Update search.js

--- js/search.js
+++ js/search.js
@@ -1,3 +1,24 @@
 const search = () => {
-    
+    let ajaxCountNum = 0;
+    let ajaxLastNum = ajaxCountNum;
+
+    const searchData = {}
+
+    $.ajax({
+        // url : "http://10.80.161.202:8080/user",
+        url : "https://coatcode.herokuapp.com/user",
+        type : "GET",
+        data : JSON.stringify(searchData),
+        beforeSend : function(xhr){
+            ajaxCountNum = ajaxCountNum+1;
+            xhr.setRequestHeader("Content-type","application/json");
+            xhr.setRequestHeader("Authorization",`Bearer ${sessionStorage.getItem('userAccessToken')}`);
+        },
+        success : function(res){
+           console.log(res);
+        },
+        error : function(res){
+            console.log(res);
+        }
+    });
 }
