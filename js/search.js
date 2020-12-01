a0a9583521ef57c3181bb11c806804301999d392
jundogown <ns8708ns@naver.com>
2020-12-01 14:06:55 +0900

    Update search.js

--- js/search.js
+++ js/search.js
@@ -1,25 +1,33 @@
 const search = () => {
-    let ajaxCountNum = 0;
-    let ajaxLastNum = ajaxCountNum;
+    const count = 1;
+    $(document).on('keypress', '#searchbar', function(e) {
+        if(e.keyCode == 13) {
+            const searchItem = $('#searchbar').val();
+            const base = searchItem + '/' + count;
+            $.ajax({
+                url : 'http://localhost:8080/api/v1/movies/' + base,
+                type : 'GET',
+                beforeSend : function() {
 
-    const searchData = $(".search").val();
-
-    $.ajax({
-        // url : "http://10.80.161.202:8080/user",
-        url : "https://coatcode.herokuapp.com/user",
-        type : "GET",
-        data : JSON.stringify(searchData),
-        beforeSend : function(xhr){
-            ajaxCountNum = ajaxCountNum+1;
-            xhr.setRequestHeader("Content-type","application/json");
-            xhr.setRequestHeader("Authorization",`Bearer ${sessionStorage.getItem('userAccessToken')}`);
-        },
-        success : function(res){
-           console.log(res);
-           res.map()
-        },
-        error : function(res){
-            console.log(res);
+                },
+                success : function(res) {
+                    console.log(res);
+                    $('.slideLayout').hide();
+                    $('.popularMovie').empty();
+                    let alt;
+                    res.items.map((item, index) => {
+                        item.title = item.title.replace('<b>','');
+                        item.title = item.title.replace('</b>','');
+                        item.director = item.director.replace('|', '');
+                        alt = base + '/' + index;
+                        $('.popularMovie').append(`    
+                            <div class="popImg">
+                                <img src="${item.image}" title="${alt}" alt="${item.title}" onclick="getMovieInfo();" class="movieImg">
+                            </div>
+                        `);
+                    });
+                }
+            });
         }
-    });
+    })
 }
