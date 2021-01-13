2f40dd55f4283834bd16c13163b9e589259fffd1
jundogown <ns8708ns@naver.com>
2021-01-14 01:34:29 +0900

    Create likeList.js

---
+++ coatcode-v2/src/components/likeList.js
@@ -0,0 +1,23 @@
+import react from "react"
+
+const LikeList = () => {
+    <div className="contentContainer">
+        <div className="tumnailArea">
+            <img></img>
+        </div>
+        <div className="contentArea">
+            <span className="userName"></span>
+            <div iconArea>
+                <div className="titleArea">
+                    <span className="title"></span>
+                </div>
+                <div className="iconArea">
+                    <span className="icon"></span>
+                </div>
+            </div>
+            <span className="price"></span>
+        </div>
+    </div>
+}
+
+export default LikeList;
