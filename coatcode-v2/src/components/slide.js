b758c2077c6708ac4d2ad0ac7f6d92de7f80458a
jundogown <ns8708ns@naver.com>
2021-01-14 01:34:32 +0900

    Create slide.js

---
+++ coatcode-v2/src/components/slide.js
@@ -0,0 +1,17 @@
+import react from 'react';
+import { Route, Link } from 'react-router-dom';
+
+const Store = () => {
+    return(
+        <div class="slide">
+            <ul>
+                <img></img>
+                <img></img>
+                <img></img>
+                <img></img>
+            </ul>
+        </div>
+    );
+}
+
+export default Store;
