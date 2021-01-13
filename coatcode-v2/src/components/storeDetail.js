f6d8fd8b7598331d90e55c74c11bc5124a2ba27f
jundogown <ns8708ns@naver.com>
2021-01-14 01:34:35 +0900

    Create storeDetail.js

---
+++ coatcode-v2/src/components/storeDetail.js
@@ -0,0 +1,19 @@
+import react from 'react'
+
+const StoreDetail = () => {
+    return(
+        <div className="contentContainer">
+            <div className="imgContainer">
+                <img className="tumnailPicture"></img>
+            </div>
+            <div className="additionalImg_Area">
+                <img className="additionalImg"></img>
+                <img className="additionalImg"></img>
+                <img className="additionalImg"></img>
+                <img className="additionalImg"></img>
+                <img className="additionalImg"></img>
+            </div>
+        </div>
+        
+    );
+}
