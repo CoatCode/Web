b05d28e7d3d592bfd000504ceea7e4e3ac0063a9
jundogown <ns8708ns@naver.com>
2021-01-14 18:09:58 +0900

    Update storeDetail.js

--- coatcode-v2/src/components/storeDetail.js
+++ coatcode-v2/src/components/storeDetail.js
@@ -13,7 +13,25 @@ const StoreDetail = () => {
                 <img className="additionalImg"></img>
                 <img className="additionalImg"></img>
             </div>
+            <div className="userInfo">
+                <div className="profileArea">
+                    <img className="profile"></img>
+                </div>
+                <div className="userNameArea">
+                    <span className="userName"></span>
+                </div>
+            </div>
+            <div className="goodsInfo">
+                <span className="goodsName"></span>
+                <span className="price"></span>
+            </div>
+            <div className="iconArea">
+                <img className="heartIcon"></img>
+                <img className="pokeIcon"></img>
+            </div>
+            <div className="btnArea">
+                <button className="purchaseBtn"></button>
+            </div>
         </div>
-        
     );
 }
